e5a21571a564e501ae847ff771c6ac5195b0d21e
deamon <silentvegatables@gmail.com>
2024-01-14 12:58:07 +0300

    added some resource methods to WorkerController

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,17 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Worker;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
     public function index()
     {
-        return 'index action of Worker Controller';
+        return Worker::all();
     }
 
-    public function show()
+    public function show(int $id): Worker
     {
-        return 'show action of Worker Controller';
+        return Worker::find($id);
+    }
+
+    public function create(): string
+    {
+        $workers = [
+            'name' => 'Dima',
+            'surname' => 'Barabanov',
+            'email' => 'dima@mail.ru',
+            'age' => '23',
+            'description' => 'No description',
+            'is_married' => 'false',
+        ];
+        Worker::create($workers);
+
+        return 'Worker was created';
+    }
+
+    public function update(int $id): string
+    {
+        $worker = Worker::first();
+
+        $worker->update([
+            'name' => 'Deamon',
+            'surname' => 'Barbarian'
+        ]);
+
+        return "Worker $worker->name updated successfully";
+    }
+
+    public function destroy(int $id): string
+    {
+        $worker = Worker::find($id);
+        if(!is_null($worker)) {
+            $workerName = $worker->name;
+            $worker->delete();
+            return "Worker $workerName was deleted successfully";
+        }
+        return "Worker with id = $id was not found";
     }
 }
