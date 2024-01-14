6c026b5084e8216ec1c87f94dc81e19ce02d038f
deamon <silentvegatables@gmail.com>
2024-01-14 10:53:08 +0300

    Added resource route to index and show methods

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\WorkerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index']);
-Route::get('/show', [\App\Http\Controllers\WorkerController::class, 'show']);
+//Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index']);
+//Route::get('/show', [\App\Http\Controllers\WorkerController::class, 'show']);
+Route::resource('workers', WorkerController::class)->only('index', 'show');
