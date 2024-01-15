bcdd6ef0191fcb472f5cd9e9c9a6b587861731f7
deamon <silentvegatables@gmail.com>
2024-01-15 15:51:45 +0300

    Create store method, did some refactoring

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,36 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Worker\StoreRequest;
 use App\Models\Worker;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
         $workers =  Worker::all();
         return view('worker.index', compact('workers'));
     }
 
-    public function show(int $id): Worker
+    public function show(Worker $worker): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        return Worker::find($id);
+        //$worker = Worker::find($id);
+        return view('worker.show', compact('worker'));
     }
 
-    public function create(): string
+    public function create()
     {
-        $workers = [
-            'name' => 'Dima',
-            'surname' => 'Barabanov',
-            'email' => 'dima@mail.ru',
-            'age' => '23',
-            'description' => 'No description',
-            'is_married' => 'false',
-        ];
-        Worker::create($workers);
+        return view('worker.create');
+    }
 
-        return 'Worker was created';
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $data['is_married'] = isset($data['is_married']);
+        Worker::create($data);
+
+        return redirect()->route('workers.index');
     }
 
     public function update(int $id): string
