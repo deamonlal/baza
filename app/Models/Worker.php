cf16853e57cf2f23cee3a1e5c6739ad3442dc7b4
deamon <silentvegatables@gmail.com>
2024-01-15 17:00:01 +0300

    Install ide_helper to make phpDOC to laravel classes. Before that, PHPStorm couldn't find some methods on its own

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -5,6 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Worker
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $surname
+ * @property string $email
+ * @property int|null $age
+ * @property string|null $description
+ * @property int $is_married
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereAge($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereIsMarried($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereSurname($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Worker whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Worker extends Model
 {
     use HasFactory;
