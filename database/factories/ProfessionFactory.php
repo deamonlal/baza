b0b88eefafc8a819c84085ca3ea965ec141ab3f5
deamon <silentvegatables@gmail.com>
2024-01-21 16:28:27 +0300

    Added factories and seeders

---
+++ database/factories/ProfessionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profession>
+ */
+class ProfessionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle,
+            'department_id' => Department::inRandomOrder()->first()->id,
+            'responsibilities' => fake()->text()
+        ];
+    }
+}
---
+++ database/seeders/ProfessionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profession;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProfessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Profession::factory(7)->create();
+    }
+}
